b840a4a61e57161743e010b4cc828bea0528a1fd
ElianaSalazar-Ruiz <eliana.salazar@est.digital.edu.co>
2021-04-17 20:58:56 -0500

    Envio del Back - end Completo

--- src/main/java/co/edu/iudigital/app/service/iface/UserService.java
+++ src/main/java/co/edu/iudigital/app/service/iface/UserService.java
@@ -1,5 +1,20 @@
 package co.edu.iudigital.app.service.iface;
 
+import co.edu.iudigital.app.model.entity.User;
+
 public interface UserService {
+	/**
+	 * crear nuevo usuario
+	 * @param user
+	 */
+	public void create(User user);
+	/**
+	 * Se loguea un usuario
+	 * @param user
+	 * @return
+	 * @throws Exception
+	 */
+	public User login(User user) throws Exception;
+	
 
 }
